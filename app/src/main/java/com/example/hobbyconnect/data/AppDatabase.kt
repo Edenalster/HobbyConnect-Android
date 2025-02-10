3e47cfce2d8e056be93f222fda6c660e1936c29c
YoavErel <yoav.e95@gmail.com>
2025-02-10 16:52:10 +0200

    project v1

--- app/src/main/java/com/example/hobbyconnect/data/AppDatabase.kt
+++ app/src/main/java/com/example/hobbyconnect/data/AppDatabase.kt
@@ -5,12 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [PostEntity::class, FollowerEntity::class, UserEntity::class], version = 2, exportSchema = false)
+@Database(entities = [PostEntity::class, FollowerEntity::class, UserEntity::class, UserLikesEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun postDao(): PostDao
     abstract fun followerDao(): FollowerDao
     abstract fun userDao(): UserDao
+    abstract fun userLikesDao(): UserLikesDao
 
 
     companion object {
